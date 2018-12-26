ecd85ca44c83d511570614e758a7971d95469e87
Sven Over <sven@svenover.de>
2018-12-26 19:30:03 +0000

    Add long descriptions and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, Extension
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="pyimmutable",
     version="0.1",
     description="Immutable dict and lists",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/spacedentist/pyimmutable",
     author="Sven Over",
     author_email="sp@cedenti.st",
@@ -23,5 +28,11 @@ setup(
             libraries=["ssl"],
             extra_compile_args=["-std=c++17"],
         )
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
 )

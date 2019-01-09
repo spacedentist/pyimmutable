951b73e8523b0b00ec936199a783a2a8c3a84616
Sven Over <sven@svenover.de>
2019-01-09 09:18:17 +0000

    Register Immutable{Dict,List} with appropriate collections.abc types

--- pyimmutable/__init__.py
+++ pyimmutable/__init__.py
@@ -1,1 +1,9 @@
+import collections.abc
+
 from _pyimmutable import ImmutableDict, ImmutableList
+
+__all__ = "ImmutableDict", "ImmutableList"
+
+
+collections.abc.Mapping.register(ImmutableDict)
+collections.abc.Sequence.register(ImmutableList)

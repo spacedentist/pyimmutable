7fda75ae1dcc1221e3608bf7c1c8f65fbce1a73b
Sven Over <sven@svenover.de>
2019-01-28 20:32:08 +0000

    Release version 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pyimmutable",
-    version="0.1.1",
+    version="0.1.2",
     description="Immutable dict and lists",
     long_description=long_description,
     long_description_content_type="text/markdown",

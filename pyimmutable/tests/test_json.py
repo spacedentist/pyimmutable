e63e271b3442506144ef204e63b164857d7cb9fb
Sven Over <sven@svenover.de>
2019-01-28 18:40:21 +0000

    Fix ImmutableDict instance count in unit test (test_dict_stress.py)
    Unit tests should not store Immutable{Dict,List} in any global variables
    so that we can make tests that check the instance count.

--- pyimmutable/tests/test_json.py
+++ pyimmutable/tests/test_json.py
@@ -23,33 +23,35 @@ class TestJson(unittest.TestCase):
     }
     """
 
-    example_immutable_data = ImmutableDict(
+    example_mutable_data = json.loads(example_json)
+
+    def test_loads(self):
+        d_from_j = json_loads(self.example_json)
+        self.assertTrue(d_from_j is example_immutable_data())
+
+    def test_load(self):
+        d_from_j = json_load(io.StringIO(self.example_json))
+        self.assertTrue(d_from_j is example_immutable_data())
+
+    def test_dumps(self):
+        j_from_d = json_dumps(example_immutable_data())
+        self.assertEqual(json.loads(j_from_d), self.example_mutable_data)
+
+    def test_dump(self):
+        f = io.StringIO()
+        json_dump(example_immutable_data(), f)
+        j_from_d = f.getvalue()
+        self.assertEqual(json.loads(j_from_d), self.example_mutable_data)
+
+
+def example_immutable_data():
+    return ImmutableDict(
         foo=1,
         bar=ImmutableDict(baz=99),
         xxx=ImmutableList((None, True, ImmutableList())),
         yyy=ImmutableDict(),
         zzz=None,
     )
-
-    example_mutable_data = json.loads(example_json)
-
-    def test_loads(self):
-        d_from_j = json_loads(self.example_json)
-        self.assertTrue(d_from_j is self.example_immutable_data)
-
-    def test_load(self):
-        d_from_j = json_load(io.StringIO(self.example_json))
-        self.assertTrue(d_from_j is self.example_immutable_data)
-
-    def test_dumps(self):
-        j_from_d = json_dumps(self.example_immutable_data)
-        self.assertEqual(json.loads(j_from_d), self.example_mutable_data)
-
-    def test_dump(self):
-        f = io.StringIO()
-        json_dump(self.example_immutable_data, f)
-        j_from_d = f.getvalue()
-        self.assertEqual(json.loads(j_from_d), self.example_mutable_data)
 
 
 if __name__ == "__main__":
--- pyimmutable/tests/test_make_immutable.py
+++ pyimmutable/tests/test_make_immutable.py
@@ -17,25 +17,27 @@ class TestMakeImmutable(unittest.TestCase):
         "zzz": None,
     }
 
-    example_immutable_data = ImmutableDict(
+    def test_make_immutable(self):
+        i_from_m = make_immutable(self.example_mutable_data)
+        self.assertTrue(i_from_m is example_immutable_data())
+        i_from_i_from_m = make_immutable(i_from_m)
+        self.assertTrue(i_from_i_from_m is i_from_m)
+
+    def test_make_mutable(self):
+        m_from_i = make_mutable(example_immutable_data())
+        self.assertEqual(m_from_i, self.example_mutable_data)
+        m_from_m_from_i = make_mutable(m_from_i)
+        self.assertEqual(m_from_m_from_i, m_from_i)
+
+
+def example_immutable_data():
+    return ImmutableDict(
         foo=1,
         bar=ImmutableDict(baz=99),
         xxx=ImmutableList((None, True, ImmutableList())),
         yyy=ImmutableDict(),
         zzz=None,
     )
-
-    def test_make_immutable(self):
-        i_from_m = make_immutable(self.example_mutable_data)
-        self.assertTrue(i_from_m is self.example_immutable_data)
-        i_from_i_from_m = make_immutable(i_from_m)
-        self.assertTrue(i_from_i_from_m is i_from_m)
-
-    def test_make_mutable(self):
-        m_from_i = make_mutable(self.example_immutable_data)
-        self.assertEqual(m_from_i, self.example_mutable_data)
-        m_from_m_from_i = make_mutable(m_from_i)
-        self.assertEqual(m_from_m_from_i, m_from_i)
 
 
 if __name__ == "__main__":
